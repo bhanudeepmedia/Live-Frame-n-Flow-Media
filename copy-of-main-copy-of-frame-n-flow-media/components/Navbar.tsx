94bf2c9e1ce7c6db0a8705f4cbbcbb79e49f53a3
Bhanu Deep Reddy <bhanudeep@Bhanus-MacBook-Air.local>
2026-01-18 03:53:00 +0530

    style: make logo much bigger - h-16 mobile, h-20 desktop

--- copy-of-main-copy-of-frame-n-flow-media/components/Navbar.tsx
+++ copy-of-main-copy-of-frame-n-flow-media/components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar: React.FC = () => {
                 <img
                   src="/logo.png"
                   alt="Frame n Flow Logo"
-                  className="h-12 md:h-16 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
+                  className="h-16 md:h-20 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
                 />
               </NavLink>
             </div>
